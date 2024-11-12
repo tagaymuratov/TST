b43e29a21ba83376a39edace72931fbc2bcf9126
tagaymuratov <tagaymuratov@gmail.com>
2024-11-13 04:30:15 +0500

    favicon

--- tstapp/admin.py
+++ tstapp/admin.py
@@ -1,15 +1,24 @@
+from typing import Any
 from django.contrib import admin
+from django.db.models.fields.related import ForeignKey
+from django.forms.models import ModelChoiceField
+from django.http import HttpRequest
 from django.utils.html import format_html
 from .models import Brands, Categories, Images, Product, SubCats
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
   ordering = ['article']
+  search_fields = ['article']
   list_display = ('article', 'category', 'subCat', 'brand', 'available',)
   list_display_links = ('article',)
   list_per_page = 20
-  search_fields = ['article']
   list_filter = ['category__name', 'subCat__name', 'brand__name', 'available']
+
+  #def formfield_for_foreignkey(self, db_field, request, **kwargs):
+  #  if db_field.name == 'subCat':
+  #    kwargs["queryset"] = SubCats.objects.filter(name__in=['God', 'Demi God']).order_by('name')
+  #  return super().formfield_for_foreignkey(db_field, request, **kwargs)
 
 @admin.register(Categories)
 class CategoriesAdmin(admin.ModelAdmin):
