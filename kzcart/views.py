8cddf88bddeab8395b718af090003e2c4a93645e
tagaymuratov <tagaymuratov@gmail.com>
2024-11-17 22:02:03 +0500

    pagination, favicon

--- kzcart/views.py
+++ kzcart/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.http import require_POST
+from tstapp.models import Product
+from .cart import KzCart
+from .forms import CartAddProductForm
 
-# Create your views here.
+@require_POST
+def cart_add(request, product_id):
+  cart = KzCart(request)
+  product = get_object_or_404(Product, id=product_id)
+  form = CartAddProductForm(request.POST)
+  if form.is_valid():
+    cd = form.cleaned_data
+    cart.add(product=product, quantity=cd['quantity'], overrideQuantity=cd['override'])
+  return redirect('kzCart:cart_detail')
+
+
+@require_POST
+def cart_remove(request, product_id):
+  cart = KzCart(request)
+  product = get_object_or_404(Product, id=product_id)
+  cart.remove(product)
+  return redirect('kzCart:cart_detail')
+
+def cart_detail(request):
+  cart = KzCart(request)
+  return render(request, 'kzCart/detail.html', {'cart':cart})
--- tst/urls.py
+++ tst/urls.py
@@ -1,12 +1,17 @@
 from django.conf import settings
 from django.contrib import admin
+from django.contrib.staticfiles.storage import staticfiles_storage
 from django.urls import path, include
 from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('', include('tstapp.urls')),
     path('kzauth/', include('kzauth.urls', namespace='kzauth')),
+    path('kzcart/', include('kzcart.urls', namespace='kzcart')),
     path('admin/', admin.site.urls),
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('tstapp/imgs/favicon.ico'))),
+    path('favicon.svg', RedirectView.as_view(url=staticfiles_storage.url('tstapp/imgs/favicon.svg')))
 ] 
 
 if settings.DEBUG:
